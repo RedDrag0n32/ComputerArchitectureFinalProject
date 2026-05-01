9339deaf6a29922eec858abe504a36bbddaaf532
RedDrag0n32 <ty.kalscheur@gmail.com>
2026-05-01 18:15:33 -0500

    created abstract predictor class

--- Simulator/Predictors/base.py
+++ Simulator/Predictors/base.py
@@ -1,1 +1,21 @@
 #Abstract Predictor Interface
+from abc import ABC, abstractmethod
+
+class BranchPredictor(ABC):
+    
+    @abstractmethod
+    def predict(self, pc: int) -> bool:
+        #return True if branch is predicted taken.
+        ...
+
+    @abstractmethod
+    def update(self, pc: int, taken: bool, target: int) -> None:
+        #update predictor state after branch resolves
+        ...
+
+    @abstractmethod
+    def storage_bits(self) -> int:
+        #return total storage cost in bits
+        ...
+
+    
